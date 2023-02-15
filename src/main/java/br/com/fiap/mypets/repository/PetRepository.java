7eb339a3a2895ae111f2d423905a85e743b44a65
Gustavo Bezerra <gustavobz305@gmail.com>
2023-02-14 21:10:50 -0300

    Criando cadastro de pet, e refatorando logs

---
+++ src/main/java/br/com/fiap/mypets/repository/PetRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.mypets.repository;
+
+import br.com.fiap.mypets.entity.PetEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends CrudRepository<PetEntity, String> {
+}
