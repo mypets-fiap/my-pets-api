3a4dcbce5514bba4a4eb2bd480ae9888e709a43b
Henrique Renan Chang <henriquechang7@gmail.com>
2023-04-08 21:08:24 -0300

    ajuste docker file

--- src/main/java/br/com/fiap/mypets/config/SecurityConfiguration.java
+++ src/main/java/br/com/fiap/mypets/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/api/v1/auth/**", "/health")
                     .permitAll()
                 .anyRequest()
                     .authenticated()
